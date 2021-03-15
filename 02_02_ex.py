56f64e23f5e34eee2aae3a679ae9d67a68beecde
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-15 20:14:12 -0300

    Add files via upload

---
+++ 02_02_ex.py
@@ -0,0 +1,9 @@
+# terminal -> pwd -> cd Desktp/ -> cd py4e/ -> ls -> python3 02_02_ex.py
+# print('Hello everybody')
+# Book 'Python for Everybody' - Dr. Charles Severance
+# Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
+
+user = str(input('Enter your name: '))
+print('Hello', user)
+# print(f'Hello {user}')
+# you can write in this way: print('Hello {}.'.format(user))
