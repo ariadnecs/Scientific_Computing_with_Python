9ff6d2fea4312a59af7904538baf9ae6f1bb5baa
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-15 20:17:22 -0300

    Update and rename 02_02_ex.py to ex_02_02.py

--- ex_02_02.py
+++ ex_02_02.py
@@ -1,5 +1,3 @@
-# terminal -> pwd -> cd Desktp/ -> cd py4e/ -> ls -> python3 02_02_ex.py
-# print('Hello everybody')
 # Book 'Python for Everybody' - Dr. Charles Severance
 # Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them.
 
