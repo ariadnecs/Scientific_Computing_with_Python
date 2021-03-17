1f9aa8e3791e6f64a815d608844d6654ea642c9e
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-16 21:47:44 -0300

    Add files via upload

---
+++ comparison_operators.py
@@ -0,0 +1,13 @@
+x = 5
+if x == 5:
+    print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+if x >= 5:
+    print('Greater than or Equals 5')
+if x < 6:
+    print('Less than 6')
+if x <= 5:
+    print('Less than or Equals 5')
+if x != 6:
+    print('Not equal 6')
