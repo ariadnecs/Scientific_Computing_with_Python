6d00b3cccd86b7e8fc93f8d0c5e8d9193c23614d
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-26 18:42:23 -0300

    Add files via upload

---
+++ ex_05_01_v2.py
@@ -0,0 +1,17 @@
+total = 0
+count = 0
+while True:
+    user = input('Enter a number: ')
+    if user == 'done':
+        break
+    try:
+        fuser = float(user)
+    except:
+        print('Invalid value! Try again.')
+        continue
+    total = total + fuser
+    count += 1
+
+print('Sum: ', total)
+print('Count: ', count)
+print('Average: ', total / count)
