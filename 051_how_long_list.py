83499e23e730003d5415d3c01fbaa99d2e059926
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-03-30 19:24:38 -0300

    Add files via upload

---
+++ 051_how_long_list.py
@@ -0,0 +1,5 @@
+greet = 'Hello Bob'
+print(len(greet))
+
+x = [1, 2, 3, 4, 5, 6, 7, 7, 6, 5, 4, 3, 2, 1]
+print(len(x))
---
+++ 052_using_range.py
@@ -0,0 +1,6 @@
+print(range(5))
+
+friends = ['Spock', 'Kirk', 'McCoy', 'scotty']
+print(len(friends))
+
+print(range(len(friends)))
