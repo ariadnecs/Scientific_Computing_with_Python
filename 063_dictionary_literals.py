0a8d1ad21907b1da9a52daea84fb6d86032e00aa
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-07 10:08:57 -0300

    Add files via upload

---
+++ 063_dictionary_literals.py
@@ -0,0 +1,5 @@
+constants = {'Spock': 1, 'Picard': 2, 'Data': 3}
+print(constants)
+
+zero = {}
+print(zero)
---
+++ 064_couters_with_a_dictionary.py
@@ -0,0 +1,7 @@
+count = dict()
+count['csev'] = 1
+count['cwen'] = 1
+print(count)
+
+count['cwen'] = count['cwen'] + 1
+print(count)
