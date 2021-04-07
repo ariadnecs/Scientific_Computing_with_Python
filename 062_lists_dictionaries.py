66476be37677dfcd51174b64102f8652aee09b23
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-07 09:43:52 -0300

    Add files via upload

---
+++ 062_lists_dictionaries.py
@@ -0,0 +1,21 @@
+print('List')
+lst = list()
+lst.append(21)
+lst.append(183)
+
+print(lst)
+print(lst[0])
+
+lst[0] = 23
+print(lst[0])
+print(len(lst))
+
+print('\nDictionaries')
+ddd = dict()
+ddd['age'] = 21
+ddd['course'] = 182
+print(ddd)
+
+ddd['age'] = 23
+print(ddd)
+print(len(ddd))
