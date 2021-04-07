555ce7d468b85b5298ee37c934348d8e99da2ec2
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-07 09:34:44 -0300

    Add files via upload

---
+++ 061_dictionaries.py
@@ -0,0 +1,11 @@
+purse = dict()
+purse['money'] = 12
+purse['candy'] = 3
+purse['tissues'] = 75
+
+print(purse)
+print(len(purse))
+print(purse['money'])
+
+purse['candy'] = purse['candy'] + 2
+print(purse)
