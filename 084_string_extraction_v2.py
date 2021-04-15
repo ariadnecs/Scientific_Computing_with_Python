e57ea3db9d7f56cde8f538c3131f59e506eed641
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-15 10:14:58 -0300

    Update 084_string_extraction_v2.py

--- 084_string_extraction_v2.py
+++ 084_string_extraction_v2.py
@@ -1,7 +1,7 @@
 import re
 
 x = 'Science spock@startrek.com command kirk@startrek.com communications uhura@startrek.com, medical bones@startrek.com'
-y = re.findall('\S+@\S+', x)
+y = re.findall('\\S+@\\S+', x)
 print(y)
 
 y = re.findall('^Science (\\S+@\\S+)', x)
