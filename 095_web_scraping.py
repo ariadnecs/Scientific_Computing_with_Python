9f747bbb7e450ecbce4b604f8e69baa796258acb
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-19 18:52:43 -0300

    Add files via upload

---
+++ 095_web_scraping.py
@@ -0,0 +1,12 @@
+import urllib.request
+
+from bs4 import BeautifulSoup
+from urllib import request, parse, error
+
+url = input('Enter - ')
+html = urllib.request.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+tags = soup('a')
+for tag in tags:
+    print(tag.get('bref', None))
