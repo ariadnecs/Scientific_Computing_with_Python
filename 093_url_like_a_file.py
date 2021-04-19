7ac59fc0beedb871461dcee7a885dcba2275a418
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-19 18:53:34 -0300

    Add files via upload

---
+++ 093_url_like_a_file.py
@@ -0,0 +1,11 @@
+import urllib.request
+from urllib import request, parse, error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+counts = dict()
+for line in fhand:
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1
+print(counts)
