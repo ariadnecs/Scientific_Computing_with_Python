dae0c6335715d2cbf078d68a59417ffe09248aff
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-20 09:10:31 -0300

    Update 094_reading_web_page.py

--- 094_reading_web_page.py
+++ 094_reading_web_page.py
@@ -1,6 +1,5 @@
-import urllib.request
 from urllib import request, parse, error
 
-fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+fhand = request.urlopen('http://www.dr-chuck.com/page1.htm')
 for line in fhand:
     print(line.decode().strip())
