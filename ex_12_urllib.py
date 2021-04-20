460117ff12b091030d9df0d5f7c54f04f2ed8963
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-20 09:11:12 -0300

    Update ex_12_urllib.py

--- ex_12_urllib.py
+++ ex_12_urllib.py
@@ -1,6 +1,5 @@
-import urllib.request
 from urllib import request, parse, error
 
-fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+fhand = request.urlopen('http://data.pr4e.org/romeo.txt')
 for line in fhand:
     print(line.decode().strip())
