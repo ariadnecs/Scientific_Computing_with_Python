537d5ac1582f8dda1266438b3cc126cd0108d98a
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-23 11:50:37 -0300

    Add files via upload

---
+++ 100_geoJSON.py
@@ -0,0 +1,31 @@
+import urllib.parse
+from urllib import request, parse, error
+import json
+
+serviceurl = "http://maps.googleapis.com/maps/api/geocode/json?"
+
+while True:
+    address = input('Enter location: ')
+    if len(address) < 1: break
+
+    url = serviceurl + urllib.parse.urlencode({'address': address})
+
+    print('Retrieving', url)
+    uh = urllib.request.urlopen(url)
+    data = uh.read().decode()
+    print('Retrieved', len(data), 'characters')
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        continue
+    lat = js["results"][0]["geometry"]["location"]["lat"]
+    lng = js["results"][0]["geometry"]["location"]["lng"]
+    print('lat', lat, 'lng', lng)
+    location = js["results"][0]['formatted_address']
+    print(location)
