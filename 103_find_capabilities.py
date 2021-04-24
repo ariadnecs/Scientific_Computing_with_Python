3e61986dafa4661a59c4fa14b6aa399521adb706
Ariadne de Castro Silvério <57275036+ariadnecs@users.noreply.github.com>
2021-04-24 11:39:08 -0300

    Add files via upload

---
+++ 103_find_capabilities.py
@@ -0,0 +1,9 @@
+st = list()
+# class 'list'
+print(type(st))
+# capabilities of 'st', in this case 'list()'
+print(dir(st))
+
+voyager = 'Star Trek Voyager'
+# in this case, capabilities of a string
+print(dir(voyager))
